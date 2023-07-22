e3bbeb398a1f760d0ad1cc3e63631bfbb85d255f
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-07-22 15:47:44 +0300

    insertion sort finally --__--

--- sort.h
+++ sort.h
@@ -23,4 +23,5 @@ void print_list(const listint_t *list);
 void swap(int *x, int *y);
 /* -------------- sorting function --------------*/
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 #endif
