2ee6905a417cb425f4232c13ab372d2704bb6efe
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2023-07-23 14:18:19 +0300

    try_task4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,33 +7,40 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	int i, j, h = 1, temp, flag = 0;
+	int i, j, h = 1;
 
 	if (!array || size < 2)
 		return;
-	while (h < (int)size)
+	while (h < ((int)size / 3))
 		h = 3 * h + 1;
 
 	while (h >= 1)
 	{
 		for (i = h; i < (int)size; i++)
 		{
-			temp = array[i];
 			j = i;
 
-			while (j >= h && temp < array[j - h])
+			while (j >= h && array[j] < array[j - h])
 			{
-				array[j] = array[j - h];
+				swap(&array[j], &array[j - h]);
 				j -= h;
-				flag = 1;
 			}
-			array[j] = temp;
 		}
 		h /= 3;
-		if (flag == 1)
-		{
-			print_array(array, size);
-			flag = 0;
-		}
+		print_array(array, size);
+	}
 }
+
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
