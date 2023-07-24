0251d1c31edc51eaa4c9b0a90b16252c636c5160
Emad Anwer <emadanwer.official@gmail.com>
2023-07-24 11:48:03 +0300

    fix swap

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -72,14 +72,19 @@ void cocktail_sort_list(listint_t **list)
 int swap_nodes(listint_t **list, listint_t *n1, listint_t *n2)
 {
 	n1->next = n2->next;
-	n2->prev = n1->prev;
+
 	if (n2->next != NULL)
 		n2->next->prev = n1;
+
 	n2->next = n1;
-	n1->prev = n2;
-	if (n2->prev != NULL)
-		n2->prev->next = n2;
+	n2->prev = n1->prev;
+
+	if (n1->prev != NULL)
+		n1->prev->next = n2;
 	else
 		*list = n2;
+
+	n1->prev = n2;
+
 	return (1);
 }
